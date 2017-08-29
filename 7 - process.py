bf18102a47d0d02377388fc4d6f628e4e77180cc
liuding <liuding@ifmuse.com>
2017-08-29 17:21:48 +0800

    add python process test

---
+++ 7 - process.py
@@ -0,0 +1,18 @@
+
+
+#!/usr/bin/python
+
+# process test
+
+a = 5
+
+if a == 1 :
+    print 'a values 1'
+elif a == 2:
+    print 'a values 2'
+elif a == 3:
+    print 'a values 3'
+elif a == 5:
+    print 'a values 5'
+else:
+    print 'not match!'
