535e7e12cd6a629a092c66ad36d4ee7e2232c4db
liuding <liuding@ifmuse.com>
2017-08-29 17:41:57 +0800

    add pass explains

---
+++ 9 - pass.py
@@ -0,0 +1,15 @@
+
+
+#!/usr/bin/python
+
+# pass sentence is only a blank block
+
+for letter in 'python':
+    if letter == 'y':
+        pass
+        print "this is a pass block"
+    else:
+        print "letter is " , letter
+else:
+    print 'loop finishs!'
+
