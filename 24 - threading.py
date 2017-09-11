34f6e9e4ba2a7d202c78b46c0722aa6f08bfd7f7
liuding <liuding@ifmuse.com>
2017-09-11 12:09:35 +0800

    update threading

--- 24 - threading.py
+++ 24 - threading.py
@@ -6,30 +6,36 @@
 import threading
 import time
 
-class CreateThread(threading.Thread):
-    threadNum = 0
-    def __init__(self , threadId , delay , count):
+class myThread(threading.Thread):
+    __threadNum = 0
+
+    def __init__(self , threadId , threadName , delay):
         threading.Thread.__init__(self)
         self.threadId = threadId
+        self.threadName = threadName
         self.delay = delay
-        self.count = count
-        CreateThread.threadNum += 1
+        myThread.__threadNum += 1
 
     def run(self):
-        print "Starting " + self.name
-        print_time(self.name, self.delay , 5)
-        print "Exiting " + self.name
+        print 'Start thread : ' ,  self.threadName
+        print_time(self.threadName , self.delay , 5)
+        print 'Exit thread : ' , self.threadName
+
 
 def print_time(threadName , delay , maxCounter):
     count = 0
-    while count < maxCounter:
+    while(count <= maxCounter):
         time.sleep(delay)
+        print '%s , %s ' % (threadName , time.ctime(time.time()))
         count += 1
-        print "%s : %s " % (threadName , time.ctime(time.time()))
+
+try:
+    thread1 = myThread(1 , 'thread-1' , 1)
+    thread2 = myThread(2 , 'thread-2' , 2)
+
+    thread1.start()
+    thread2.start()
+except:
+    print 'thread create failed'
 
 
-fThread = CreateThread('thread-1' , 1 , 5)
-sThread = CreateThread('thread-2' , 2 , 5)
-
-fThread.start()
-sThread.start()
